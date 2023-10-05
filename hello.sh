d8402662528102cc4de1b0b000ada1b1dc2912c7
deepakbhota <hota.deepak@gmail.com>
2023-10-05 16:12:10 +0100

    Updated hello.sh file with a new echo command

--- hello.sh
+++ hello.sh
@@ -2,3 +2,4 @@ echo "Hello  this is my 1st freestyle jenkin project"
 #We made a change to the file
 echo "Further changes were made to practice things"
 echo "Changes made during a new vm created on 4th day of week3"
+echo "A new comment to retest webhook and jenkin pipeline on day4 of week3"
