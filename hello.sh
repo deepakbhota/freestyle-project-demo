6be01706726f208fc2dde4d49f2f03ba59a16e89
deepakbhota <hota.deepak@gmail.com>
2023-10-05 16:07:55 +0100

    updated the hello.sh file after creating new vm and initiating all from zero

--- hello.sh
+++ hello.sh
@@ -1,3 +1,4 @@
 echo "Hello  this is my 1st freestyle jenkin project"
 #We made a change to the file
 echo "Further changes were made to practice things"
+echo "Changes made during a new vm created on 4th day of week3"
